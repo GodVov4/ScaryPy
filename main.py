050a45c3408b5f29b5ec1ffe798445553fcf6b63
GodVov4 <volume245@gmail.com>
2023-11-25 22:16:40 +0200

    create scrap

---
+++ main.py
@@ -0,0 +1,11 @@
+from scrapy.crawler import CrawlerProcess
+
+from scary_py.scary_py.spiders import authors, quotes
+
+
+process_authors = CrawlerProcess()
+# process_quotes = CrawlerProcess()
+process_authors.crawl(authors.AuthorsSpider)
+# process_quotes.crawl(quotes.QuotesSpider)
+process_authors.start()
+# process_quotes.start()
